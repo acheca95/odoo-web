0c0a8811cff9c9f3faee68ff1da1c7953b560f51
Antonio Checa <antonio_ch_95@hotmail.com>
2020-02-18 18:35:14 +0100

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,14 @@
+# __init__.py
+#from . odoo.osv import orm, fields
+from . import orm, fields
+
+
+class Times(orm.Model):
+    _name = 'web_example.stopwatch'
+
+    _columns = {
+        'time': fields.integer("Time", required=True,
+                               help="Measured time in milliseconds"),
+        'user_id': fields.many2one('res.users', "User", required=True,
+                                   help="User who registered the measurement")
+    }
---
+++ __manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': "Web Example",
+    'description': "Basic example of a (future) web module",
+    'category': 'Hidden',
+    'depends': ['web'],
+    'data': ['web_example.xml'],
+    'js': ['static/src/js/first_module.js'],
+    'css': ['static/src/css/web_example.css'],
+    'qweb': ['static/src/xml/web_example.xml'],
+    'test': ['static/src/tests/timer.js'],
+}
