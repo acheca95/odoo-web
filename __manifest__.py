03a8b4ca4335f892fdb212a8f602f87bd785ce2e
Antonio Checa <antonio_ch_95@hotmail.com>
2020-02-23 15:55:23 +0100

    Add files via upload

--- __manifest__.py
+++ __manifest__.py
@@ -1,11 +1,17 @@
 {
-    'name': "Web Example",
-    'description': "Basic example of a (future) web module",
-    'category': 'Hidden',
-    'depends': ['web'],
-    'data': ['web_example.xml'],
-    'js': ['static/src/js/first_module.js'],
-    'css': ['static/src/css/web_example.css'],
-    'qweb': ['static/src/xml/web_example.xml'],
-    'test': ['static/src/tests/timer.js'],
+    'name': 'web_ventas',
+    'version': '1.0',
+    'author': 'Antonio Checa',
+    'complexity': 'Facil',
+    'description': 'Datos de un taller',
+    'category': '',
+    'depends': ['website'],
+    'data': [
+    	'views/web_view.xml'
+    ],
+    'auto_install': False,
+    'installable': True,
+    'external_dependencies': {
+        'python': [],
+    },
 }
